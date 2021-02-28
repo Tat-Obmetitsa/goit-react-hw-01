6f3a501cfe9632a9816592aa13eb2814d1949e06
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2021-02-28 02:07:27 +0200

    init: add style

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import Profile from '../Profile/Profile';
+import Statistics from '../Statistics/Statistics';
+import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import Container from '../Container/Container';
+
+import user from '../../db/user.json';
+import statisticalData from '../../db/statistical-data.json';
+import friends from '../../db/friends.json';
+import transactions from '../../db/transactions.json';
+
+function App() {
+  return (
+    <Container>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+}
+export default App;
